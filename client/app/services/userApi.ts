18aba5c7629aa6f74af85804fb1f169dfe9ffe37
morningstar <mandeep7yadav@gmail.com>
2025-06-20 17:36:56 +0530

    userLevelAPI

--- client/app/services/userApi.ts
+++ client/app/services/userApi.ts
@@ -7,9 +7,9 @@ const api = axios.create({
     headers: {
       'Content-Type': 'application/json',
     },
-  });
+});
   
-  api.interceptors.request.use(
+api.interceptors.request.use(
     (config) => {
       const token = localStorage.getItem('token');
       console.log(token)
@@ -20,7 +20,72 @@ const api = axios.create({
     },
     (error) => {
       return Promise.reject(error);
-    }
-  );
+    });
 
-// Fetching APIs
+// Destination APIs
+export const destinationApi = {
+  getAllDestination : async( ) => {
+    const response = await axios.get(`${API_URL}/destinations`);
+    return response.data;
+  },
+  getDestinationById : async(id : string) => {
+    const response = await axios.get(`${API_URL}/destinations/${id}`);
+    return response.data;
+  },
+  getDestinationByName : async(name:string) => {
+    const response = await axios.get(`${API_URL}/destinations/name/${name}`);
+    return response.data;
+  },
+  searchDestination : async(query:string) => {
+    const response = await axios.get(`${API_URL}/destinations/search?query=${query}`);
+    return response.data;
+  }
+}
+
+// Tour APIs
+export const tourApi = {
+  getAllTours : async() => {
+    const response = await axios.get(`${API_URL}/tour/admin/all`);
+    return response.data;
+  },
+  getTourById : async(id : string) => {
+    const response = await axios.get(`${API_URL}/tour/${id}`);
+    return response.data;
+  },
+  getTourByDestination : async(id : string) => {
+    const response = await axios.get(`${API_URL}/tour/destination/${id}`);
+    return response.data;
+  },
+  searchTour : async(query : string) => {
+    const response = await axios.get(`${API_URL}/tour/search?q=${query}`);
+    return response.data;
+  },
+  getAvailableDates : async(id : string) => {
+    const response = await axios.get(`${API_URL}/tour/${id}/available-dates`);
+    return response.data;
+  },
+}
+
+// Booking APIs
+export const bookingApi = {
+  getUserBooking: async(id:string) => {
+    const response = await api.get(`${API_URL}/book/user/${id}`);
+    return response.data;
+  },
+  getSingleBooking : async(bookingId : string) => {
+    const response = await api.get(`${API_URL}/book/${bookingId}`);
+    return response.data;
+  },
+  createBooking : async(booking : any) => {
+    const response = await api.post(`${API_URL}/book/create-payment-order`, booking);
+    return response.data;
+  },
+  completeBooking : async(data : any) => {
+    const response = await api.post(`${API_URL}/book/complete-booking`, data);
+    return response.data;
+  },
+  cancelBooking : async(bookingId : string,reason:any) => {
+    const response = await api.put(`${API_URL}/book/${bookingId}/cancel`,reason);
+    return response.data;
+  },
+}
