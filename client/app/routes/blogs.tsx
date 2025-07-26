934b2971ebecb478df4cb6dedd90643ade6e8479
birlaPranjal <birlapranjal460@gmail.com>
2025-07-26 14:52:49 +0530

    Add new routes for About, Blogs, Contact, Cancellation, Disclaimer, Privacy Policy, Terms, and update footer links to reflect changes

--- client/app/routes/blogs.tsx
+++ client/app/routes/blogs.tsx
@@ -1,2 +1,10 @@
 // akash bhaiya is page ka ui pure project jaisa kar dena 
 //api bani hui h blogs ki server me blogroute mai dekh lena 
+
+import React from 'react'
+
+export default function Blogs() {
+  return (
+    <div>Blogs</div>
+  )
+}
--- client/app/routes/payments.tsx
+++ client/app/routes/payments.tsx
@@ -2,8 +2,8 @@
 
 import React from 'react'
 
-export default function PrivacyPolicy() {
+export default function Payments() {
   return (
-    <div>PrivacyPolicy</div>
+    <div>Payments</div>
   )
 }
